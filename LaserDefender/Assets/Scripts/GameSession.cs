28eb14e6007fea577e6ca9305a72a71f7b4e3756
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-02-25 21:32:38 +0200

    Update footer links

---
+++ LaserDefender/Assets/Scripts/GameSession.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    int score = 0;
+
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
